9790e44c890609e33d3301c03d9b14f022d88d32
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-06-16 17:48:54 +1200

    Added some styling

--- components/Contact.js
+++ components/Contact.js
@@ -2,6 +2,9 @@ function Contact() {
     return (
         <div className="flex flex-col max-w-full h-screen">
             <h1>Contact</h1>
+
+            <div className="hidden md:inline-flex bg-red-100 h-3/4 w-3/6 
+                rounded-tl-3xl rounded-tr-full rounded-br-none rounded-bl-none"></div>
         </div>
     )
 }

a9b9be803f9cb4268832875093b1a617e6179a99
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-06-17 15:37:53 +1200

    Logo Icon Changed

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,12 @@ function Header() {
         <div className="max-w-full top-0 z-50 bg-white flex items-center p-10 h-10 shadow-md">
             {/* Image */}
             <div className="flex items-center w-24 -ml-4 md:w-48 md:pl-20 lg:w-56">
-               <Image src="/logo.png" alt="me" width={163} height={30} />
+                <div className="hidden md:inline-flex" >
+                    <Image src="/logo.png" alt="me" width={163} height={30} />
+                </div>
+                <div className="inline-flex md:hidden" >
+                    <Image src="/logo_small.png" alt="me" width={21} height={13} />
+               </div>
             </div>
             {/* Right */}   
             <div className="flex items-center sm:space-x-2 justify-end absolute right-0">

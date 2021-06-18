66222fb96a186973c6b91c8154adea852e8fa550
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-06-18 14:13:54 +1200

    Form not posting

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,36 @@
 import Image from "next/image";
 import Pulse from 'react-reveal/Pulse';
 import RubberBand from 'react-reveal/RubberBand';
-
+import { useForm } from 'react-hook-form';
+import axios from 'axios'
+// import { useToast } from '../hooks/useToast';
 
 function Contact() {
+    const { register, handleSubmit, errors, reset } = useForm();
+
+    async function onSubmitForm(values) {
+        let config = {
+          method: 'post',
+          url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          data: values,
+        };
+
+        try {
+            const response = await axios(config);
+            console.log(response);
+            if (response.status == 200) {
+            reset();
+            toast(
+                'success',
+                'Thank you for contacting us, we will be in touch soon.'
+            );
+            }
+        } catch (err) {}
+    }
+
     return (
         <div className="flex flex-row max-w-full h-screen" id="contact">
             <div className="hidden object-contain lg:inline-flex items-center justify-center w-4/6 bg-red-500">
@@ -21,12 +48,14 @@ function Contact() {
                 {/* Details */}
                 <div className="flex flex-col z-10 justify-evenly">
                     <div className="my-3">
-                        <input type="text" placeholder="Your Name" className="bg-red-100 text-xl font-thin p-4 border-transparent w-96 my-3" />
-                        <input type="email" placeholder="Your Email" className="bg-red-100 text-xl font-thin p-4 border-transparent w-96 my-3" />
-                        <textarea placeholder="Project Details" style={{ resize: 'none' }} className="bg-red-100 text-xl font-thin p-4 border-transparent w-96 h-40 my-3" />
-                        <div className="flex items-center justify-center bg-red-500 p-4 my-3 w-56 cursor-pointer">
-                            <button className="text-white text-lg font-medium">Send</button>
-                        </div>
+                        <form method="post" onSubmit={handleSubmit(onSubmitForm)}>
+                            <input type="text" {...register('name', { required: true })} placeholder="Your Name" className="bg-red-100 text-xl font-thin p-4 border-transparent w-96 my-3" />
+                            <input type="email" {...register('email', { required: true })} placeholder="Your Email" className="bg-red-100 text-xl font-thin p-4 border-transparent w-96 my-3" />
+                            <textarea {...register('message', { required: true })} placeholder="Project Details" style={{ resize: 'none' }} className="bg-red-100 text-xl font-thin p-4 border-transparent w-96 h-40 my-3" />
+                            <div className="">
+                                <button className="flex items-center justify-center bg-red-500 p-4 my-3 w-56 cursor-pointer text-white text-lg font-medium">Send</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
                 </RubberBand>   
@@ -34,6 +63,5 @@ function Contact() {
         </div>
     )
 }
-
 export default Contact
 

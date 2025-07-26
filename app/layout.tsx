f4dc70dc0cc247f54e6c426f0839c519ff6ee16c
Sheil Gandhi <sheil.gandhi@mobihq.com>
2025-07-26 18:32:32 -0400

    feat: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme/theme-provider';
 
@@ -30,6 +31,7 @@ export default function RootLayout({
                 data-gr-ext-installed=""
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
+                <Analytics />
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
